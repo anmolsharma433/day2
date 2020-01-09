09d6490725bfaa5220ee89cd3f94de1e32162134
anmol sharma <anmolsharma433@gmail.com>
2020-01-09 15:27:38 -0500

    mapview implemented

--- day2/ViewController.swift
+++ day2/ViewController.swift
@@ -7,12 +7,34 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // define 1st lat and long
+        let latitude : CLLocationDegrees = 43.64
+        let longitude : CLLocationDegrees = -79.38
+        
+        //define delta Diff of lat and long
+        let latDelta : CLLocationDegrees = 0.05
+        let longDelta : CLLocationDegrees = 0.05
+        
+        //define span
+        let span = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: longDelta)
+        
+        //Define Location
+        let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        
+        //define region
+        let region = MKCoordinateRegion(center: location, span: span)
+        
+        //set region on mapView
+        mapView.setRegion(region, animated: true)
     }
 
 
