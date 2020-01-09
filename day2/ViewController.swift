6d58ebd21aacefc30a50aed01c22991a197a2444
anmol sharma <anmolsharma433@gmail.com>
2020-01-09 15:14:41 -0500

    Initial Commit

---
+++ day2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  day2
+//
+//  Created by Anmol Sharma on 2020-01-09.
+//  Copyright © 2020 anmol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
