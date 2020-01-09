0b830ef62838117115951d48a359dc60c782184a
anmol sharma <anmolsharma433@gmail.com>
2020-01-09 15:58:12 -0500

    map annotation with gesture

--- day2/ViewController.swift
+++ day2/ViewController.swift
@@ -17,12 +17,12 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         // define 1st lat and long
-        let latitude : CLLocationDegrees = 43.64
-        let longitude : CLLocationDegrees = -79.38
+        let latitude : CLLocationDegrees = 30.9331
+        let longitude : CLLocationDegrees = 74.610214
         
         //define delta Diff of lat and long
-        let latDelta : CLLocationDegrees = 0.05
-        let longDelta : CLLocationDegrees = 0.05
+        let latDelta : CLLocationDegrees = 24.3588
+        let longDelta : CLLocationDegrees = 36.7704
         
         //define span
         let span = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: longDelta)
@@ -35,6 +35,27 @@ class ViewController: UIViewController {
         
         //set region on mapView
         mapView.setRegion(region, animated: true)
+        
+        //adding annotation for the map
+        let annotation = MKPointAnnotation()
+        annotation.title = "Ferozepur"
+        annotation.subtitle = "picho krde belong mitro Ferozpur shehar to."
+        annotation.coordinate = location
+        mapView.addAnnotation(annotation)
+        
+        //Long press Gesture
+        let uiLogr = UILongPressGestureRecognizer(target: self, action: #selector(longPress))
+        mapView.addGestureRecognizer(uiLogr)
+    }
+    
+    @objc func longPress(gestureRecognizer : UILongPressGestureRecognizer)
+    {
+        let touchPoint = gestureRecognizer.location(in: mapView)
+        let coordinator = mapView.convert(touchPoint, toCoordinateFrom: mapView)
+        let annotation = MKPointAnnotation()
+        annotation.title = "Lat \(coordinator.latitude) & long\(coordinator.longitude)"
+        annotation.coordinate = coordinator
+        mapView.addAnnotation(annotation)
     }
 
 
